c8bc9685bca0b9146dc7e9b40088fafcb9fa2e7b
Massimo Gengarelli <gengarel@cs.unibo.it>
2011-12-13 00:29:26 +0100

    Use SCons instead of autotools.

---
+++ SConfig.py
@@ -0,0 +1,22 @@
+# THIS FILE WAS AUTOMATICALLY GENERATED. MANUAL CHANGES IN THIS FILE WILL BE LOST AFTER THE NEXT C/C++ PROJECT CHANGES!!!
+SCONS_OPTIONS = {'num_jobs':'16'}
+DECIDER = 'MD5'
+COMP_INCLUDES_INTO_CCFLAGS = False
+BUILD_ARTIFACT_NAME = 'libssh2Wrapper'
+PROJECT_TYPE = 'exe'
+BUILD_CONFIGURATION = 'Debug'
+TOOLCHAIN_NAME = 'macosx gcc'
+C_FLAGS = '-O0 -g3 -Wall -c -fmessage-length=0'
+CXX_FLAGS = '-DDEBUG -I/usr/local/include -O0 -g3 -Wall -c -fmessage-length=0'
+COMPILER_NAME = 'g++'
+PROJECT_NAME = 'libssh2Wrapper'
+INCLUDES = ['/usr/local/include']
+LIBRARIES = ['ssh2']
+LIBRARY_PATHS = ['/usr/local/include']
+SOURCE_PATHS = {'src':[], 'libssh2Wrapper':['src']}
+PRE_BUILD_COMMAND = ''
+PRE_BUILD_DESC = ''
+POST_BUILD_COMMAND = ''
+POST_BUILD_DESC = ''
+COMPILER_DEFINES = {'DEBUG':''}
+LINKER_FLAGS = ''
